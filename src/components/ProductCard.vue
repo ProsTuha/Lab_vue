bf7b09ac7960dfec5d3f7256c02de2a0b23ecc13
ProsTuha <tuhalf2002@mail.ru>
2022-01-19 13:21:48 +0300

    Product detailing

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,21 +1,21 @@
 <template>
-<div class="product-card">
+  <div class="product-card">
     <div class="product-card__front">
-      <img class="product-card__front-logo" 
+      <img class="product-card__front__logo" 
       :src="require('@/img/products/card/'+ product.productImagePath)" 
       :alt="product.productName">
-      <div class="product-card__front-info">
-        <div class="product-card__front-name">
-          {{product.productName}}
+      <div class="product-card__front__info">
+        <div class="product-card__front__info__name">
+          {{productName}}
         </div>
-        <div class="product-card__front-price">
+        <div class="product-card__front__info__price">
           {{product.productPrice + '$'}}
         </div>
-        <div class="product-card__front-genre">
+        <div class="product-card__front__info__genre">
           {{product.productGenre}}
         </div>
-        <div class="product-card__front-rating">
-          <div class="product-card__front-rating-star" 
+        <div class="product-card__front__info__rating">
+          <div class="product-card__front__info__rating-star" 
           v-for="star in raitingStars.slice(0, product.productRating)" :key="star">
             {{star}}
           </div>
@@ -23,13 +23,13 @@
       </div>
     </div>
     <div class="product-card__back">
-      <router-link class="product-card__back-link" :to="'/products/product-info'">
-      <div class="product-card__back-description">
+      <router-link class="product-card__back__link" :to="'/product/' + product.id">
+      <div class="product-card__back__description">
         {{product.productDescription}}
       </div>
       </router-link>
-      <div class="product-card__back-adding">
-        <button class="product-card__back-adding-button" href="#">
+      <div class="product-card__back__add-to-cart">
+        <button class="product-card__back__add-to-cart__button" href="#">
           Add to cart
         </button>
       </div>
@@ -43,6 +43,14 @@ import { Options, Vue } from 'vue-class-component';
 @Options({
   props: {
     product: Object
+  },
+  computed: {
+    productName() {
+      if (this.product.productName.length > 10) {
+        return `${this.product.productName.substr(0, 10)}...`
+      }
+      return this.product.productName
+    }
   }
 })
 
@@ -53,26 +61,25 @@ export default class ProductCard extends Vue {
 
 <style lang="scss" scoped>
 .product-card {
-  position: relative;
   width: 100%;
   height: 100%;
   perspective: 1000px;
-  margin: 0 auto 20px;
-
-    &__front {
-      position: absolute;
-      background-color: $color-black;
-      height: 100%;
-      width: 100%;
-      backface-visibility: hidden;
-      transition: 1s;
-      margin: 0 auto;
-      font-size: 1.1em;
-      color: $color-white;
-      overflow: hidden;
-    }
-
-    &__front-logo {
+  margin-bottom: 20px;
+  position: relative;
+      
+  &__front {
+    position: absolute;
+    background-color: $color-black;
+    height: 100%;
+    width: 70%;
+    backface-visibility: hidden;
+    transition: 1s;
+    margin: 0 auto;
+    font-size: 1.1em;
+    color: $color-white;
+    overflow: hidden;
+  
+    &__logo {
       display: block;
       width: 100%;
       height: 80%;
@@ -81,40 +88,41 @@ export default class ProductCard extends Vue {
       object-fit: cover;
     }
 
-    &__front-info {
+    &__info {
       padding: 1% 4% 1%;
       position: relative;
-    }
 
-    &__front-name {
-      color: $color-purple;
-      font-weight: 600;
-    }
+      &__name {
+        color: $color-purple;
+        font-weight: 600;
+      }
           
-    &__front-name,
-    &__front-price {
-      float: left;
-      width: 50%;
-    }
+      &__name,
+      &__price {
+        float: left;
+        width: 50%;
+      }
 
-    &__front-price {
-      text-align: right;
-    }
+      &__price {
+        text-align: right;
+      }
 
-    &__front-genre {
-      font-size: 0.9em;
-      font-weight: lighter;
-    }
+      &__genre {
+        font-size: 0.9em;
+        font-weight: lighter;
+      }
 
-    &__front-rating-star {
-      display: inline-block;
-      color: $rating-star;
+      &__rating-star {
+        display: inline-block;
+        color: $rating-star;
+      }
     }
+  }
 
   &__back {
     position: absolute;
     top: 0;
-    width: 100%;
+    width: 70%;
     height: 100%;
     margin: 0 auto;
     background-color: $color-black;
@@ -124,23 +132,28 @@ export default class ProductCard extends Vue {
     text-align: center;
     font-size: 1em;
     font-weight: 800;
-  }
+      
+    &__add-to-cart {
+      position: absolute;
+      width: 100%;
+      top: 85%;
 
-  &__back-adding {
-    position: absolute;
-    width: 100%;
-    top: 85%;
-    }
-    
-    &__back-adding-button {
-      background-color: $color-purple;
-      border-radius: 5px;
-      border: 0;
+      &__button {
+        background-color: $color-purple;
+        border-radius: 5px;
+        border: 0;
+      }
     }
 
-    &__back-link {
+    &__description {
+      width: 100%;
+      height: 100%;
+    }
+
+    &__link {
       text-decoration: none;
     }
+  }
 
   &__front,
   &__back {
