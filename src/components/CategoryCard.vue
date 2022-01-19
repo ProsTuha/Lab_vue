b2240165dc05ca79052831b210a8e1ecc994e502
ProsTuha <tuhalf2002@mail.ru>
2022-01-19 13:22:38 +0300

    Product detailing

--- src/components/CategoryCard.vue
+++ src/components/CategoryCard.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="category">
-    <router-link class="category__link"  
-    :to="'/products/' + category.categoryName.toLowerCase() + '-products'">
-      <img class="category__logo" 
-      :src="require('@/img/platform-categories/'+ category.logoPath)" 
-      :alt="category.categoryName">
-      
-      <div class="category__name">
-        {{category.categoryName}}
-      </div>  
-    </router-link>
+  <div class="categories">
+    <div class="categories__platform" v-for="card in categories" :key="card.id">
+      <router-link class="categories__platform-links"  
+      :to="'/products/' + card.categoryName.toLowerCase() + '-products'">
+
+        <img class="categories__platform-links__logo" 
+        :src="require('@/img/platform-categories/'+ card.logoPath)" :alt="card.categoryName">
+
+        <div class="categories__platform-links__description">
+          {{card.categoryName}}
+        </div>
+        
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -18,10 +21,8 @@ import { Options, Vue } from 'vue-class-component'
 import { ICategory } from '@/interfaces';
 
 @Options({
-  props: {
-    category: Object
-  }
 })
+
 export default class CategoryCards extends Vue {
   categories: ICategory[] = [
     {
@@ -44,62 +45,51 @@ export default class CategoryCards extends Vue {
 </script>
 
 <style lang="scss" scoped>
-.category {
+.categories {
   display: flex;
   justify-content: space-between;
   width: 100%;
   height: 100%;
+  padding: 1% 1%;
   margin: 0 auto;
-  background-color: $color-purple;
-  text-align: center;
-  border-radius: 15px;
-  text-decoration: none;
 
-  &__link {
-    width: 98%;
-    text-decoration: none;
-    margin: 2px auto 2%;
-    background: $color-white;
+  &__platform {
+    background-color: $color-purple;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    margin: 0 5%;
+    height: 100%;
+    text-align: center;
     border-radius: 15px;
-  }
-
-  &__logo {
-    width: 25%;
-    margin: 5% auto 0;
-  }
-
-  &__name {
-    font-size: 1.5em;
-    font-weight: 500;
-    margin-bottom: 2%;
-    color: $color-black;
-  }
-
-  &:hover {
-    background-color: $color-black;
-
-    .category__link {
-      background-color: $color-purple;
-    }
-  }
-
-  &__platform-links {
-    width: 98%;
     text-decoration: none;
-    margin: 2px auto 2%;
-    background: $color-white;
-    border-radius: 15px;
 
-    &__logo {
-      width: 30%;
-      margin: 5% auto 0;
+    &-links {
+      width: 98%;
+      text-decoration: none;
+      margin: 2px auto 2%;
+      background: $color-white;
+      border-radius: 15px;
+
+      &__logo {
+        width: 25%;
+        margin: 5% auto 0;
+      }
+
+      &__description {
+        font-size: 1.5em;
+        font-weight: 500;
+        margin-bottom: 2%;
+        color: $color-black;
+      }
     }
 
-    &__description {
-      font-size: 1.5em;
-      font-weight: 500;
-      margin-bottom: 2%;
-      color: $color-black;
+    &:hover {
+      background-color: $color-black;
+
+      .categories__platform-links {
+        background-color: $color-purple;
+      }
     }
   }
 }
